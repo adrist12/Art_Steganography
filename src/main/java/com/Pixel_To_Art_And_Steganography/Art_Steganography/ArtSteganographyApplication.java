cc576dbe62789000019ad144309bd412c423c144
Adrian Acosta <adrianbeerus88@gmail.com>
2026-05-28 00:38:10 -0500

    Creacion del proyect

---
+++ src/main/java/com/Pixel_To_Art_And_Steganography/Art_Steganography/ArtSteganographyApplication.java
@@ -0,0 +1,13 @@
+package com.Pixel_To_Art_And_Steganography.Art_Steganography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSteganographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtSteganographyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pixel_To_Art_And_Steganography/Art_Steganography/ArtSteganographyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pixel_To_Art_And_Steganography.Art_Steganography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtSteganographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

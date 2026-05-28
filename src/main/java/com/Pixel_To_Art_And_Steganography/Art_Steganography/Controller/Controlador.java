5b4fc9d7ea304eb63ffdbb5732af0dbd02582bd8
Adrian Acosta <adrianbeerus88@gmail.com>
2026-05-28 00:46:15 -0500

    .

--- src/main/java/com/Pixel_To_Art_And_Steganography/Art_Steganography/Controller/Controlador.java
+++ src/main/java/com/Pixel_To_Art_And_Steganography/Art_Steganography/Controller/Controlador.java
@@ -1,4 +1,33 @@
 package com.Pixel_To_Art_And_Steganography.Art_Steganography.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class Controlador {
+    @GetMapping
+    public String get_Index(Model model) {
+        return "index";
+    }
+    @PostMapping
+    public String post_Index(Model model) {
+        return "index";
+    }
+    @GetMapping
+    public String get_Art(Model model) {
+        return "Art";
+    }
+    @PostMapping
+    public String post_Art(Model model) {
+        return "Art";
+    }
+    @GetMapping
+    public String get_Image(Model model) {
+        return "Steganography";
+    }
+    @PostMapping
+    public String post_Image(Model model) {
+        return "Steganography";
+    }
 }
